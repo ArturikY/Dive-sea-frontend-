15c1c791e9c1cc40e454efe30525a4a601c32a42
Artur <artur090806@yandex.ru>
2023-10-23 16:18:38 +0300

    sixth commit

--- src/store/api/collection.api.ts
+++ src/store/api/collection.api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { ICollection } from '../../components/screens/home/collection/Collection.interface'
 
-const API_URL = 'https://dive-sea-backend.netlify.app'
+const API_URL = 'https://dive-sea-backend.vercel.app'
 
 export const collectionApi = createApi({
 	reducerPath: 'collection_api',
